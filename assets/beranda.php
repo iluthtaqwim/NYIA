54dd844d68b1e0317d6bcfe784ec2f5525ec6917
iluthtaqwim <iluth21.i2@gmail.com>
2019-08-10 08:38:06 +0700

    Assets hari jumat

--- assets/beranda.php
+++ assets/beranda.php
@@ -1,3 +1,12 @@
 <div>
-    
+    <div style="text-align: center">
+        <img src="../assets/img/Angkasa_Pura_logo_2011.svg.png" style="width: 20%; margin-bottom: 5%" alt="" srcset="">
+    </div>
+    <div style="" class="card">
+        
+        <div class="card-body">
+            <h1 style="text-align:center " class="card-title">HISTORY</h1>
+            <p>INI TEMPAT HISTORY DATA DITAMPILKAN</p>
+        </div>
+    </div>
 </div>
--- assets/tracking.php
+++ assets/tracking.php
@@ -1,25 +1,27 @@
 <div style="text-align: center">
         <img src="../assets/img/Angkasa_Pura_logo_2011.svg.png" style="width: 20%; margin-bottom: 5%" alt="" srcset="">
-    </div>
+</div>
     <div class="card">
         <div class="card-header" style="text-align: center">
             <h1>HOME PREVIEW</h1>
         </div>
-        <div>
-            <button class="btn btn-primary" type="submit">Add User</button>
-        </div>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" style="width:10%" data-toggle="modal" data-target="#exampleModal">
+  Add Data
+</button>
         <div class="card-content">
             <table class="table table-striped">
                 <thead class="thead-inverse">
                     <tr>
-                        <th>Kontrak</th>
+                        <th>No</th>
+                        <th>Document</th>
+                        <th>Nama Kontrak</th>
+                        <th>Tanggal Contract</th>
                         <th>Sales</th>
                         <th>Legal</th>
                         <th>Sales</th>
                         <th>Legal</th>
-                        <th>GM</th>
-                        <th>Status</th>
-                        <th>Delete</th>
+
                     </tr>
                     </thead>
                     <tbody>
@@ -28,6 +30,11 @@
                             <td>TEST</td>
                             <td>TEST</td>
                             <td>TEST</td>
+                            <td>
+                                <button type="button"><img src="../assets/img/attach.png" alt="" style="width:20px"></button>
+                                <p>27-08-2019</p>
+                                <p style="font-size: 12px">INI KETERANGAN</p>
+                            </td>
                             <td>TEST</td>
                             <td>TEST</td>
                             <td>TEST</td>
@@ -36,3 +43,32 @@
             </table>
         </div>
     </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="form-group" method="post">
+          <input type="text" class="form-control" name="" id="" placeholder="Nama Mitra">
+          <br>
+          <input type="text" class="form-control" name="" id="" placeholder="Nama Perjanjian">
+          <br>
+          <input type="text" class="form-control" name="" id="" placeholder="Tanggal Kontrak">
+          <br>
+          <input type="file" accept="application/pdf" class="form-control" name="" id="" placeholder="Lampiran">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" >Simpan</button>
+      </div>
+    </div>
+  </div>
+</div>
